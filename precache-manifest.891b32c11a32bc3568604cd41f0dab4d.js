7f8f9c4471a43bf41e2c4aef513c73ef5c8eacc4
Guillaume HARARI <guillaume.harari@wildcodeschool.fr>
2020-03-17 16:55:39 +0100

    Updates

---
+++ precache-manifest.891b32c11a32bc3568604cd41f0dab4d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97842b3197ac839a0af0df9ad3a2730b",
+    "url": "/project-1-never-end/index.html"
+  },
+  {
+    "revision": "918ba2a749dbab229ffb",
+    "url": "/project-1-never-end/static/js/2.a493f40c.chunk.js"
+  },
+  {
+    "revision": "47832a8d60cb1148856a",
+    "url": "/project-1-never-end/static/js/main.3b361a48.chunk.js"
+  },
+  {
+    "revision": "d374f446d48d9740e0de",
+    "url": "/project-1-never-end/static/js/runtime~main.34296868.js"
+  }
+]);
